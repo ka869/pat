37fd21864ef74d88ca01293f51f81e4a4da7edd4
ka869 <83193583+ka869@users.noreply.github.com>
2026-06-18 11:52:55 -0500

    Add CRUD methods to TodoService
    Implemented methods to get, create, and delete todo items.

--- src/main/java/com/example/demo/todo/TodoService.java
+++ src/main/java/com/example/demo/todo/TodoService.java
@@ -10,12 +10,58 @@ import lombok.RequiredArgsConstructor;
 
 
 public class TodoService {
+
+    private final TodoRepository todoRepository;
+    
     // Task 1: code getAllItems()
+public List<Todo> getAllItems(String text){
+
+    List<Todo> todos;
+
+    if(text==null || text.isBlank()){
+   // in repository i will write the findall 
+
+        todoRepository.findAll();
+        
+    }else{
+         todos= todoRepository.findbyItemContaingIgnoreCase(text);
+          
+    }
+
+
+    return todos;
+}
+
+    
     // throw exception if no todo items exist in table
 
     // Task 2: code getItemById()
+public   Optional<Todo> getItemById(LongID){
+
+    return todoRepository.findById(id);
+                                    
+}
 
     // Task 3: code createItem()
 
+
+    public Todo CreateItem(Todo todo){
+ return todoRepository.save(todo);
+    }
+
     // Task 4: code deleteItem()
+
+
+    public boolean deleteItem(Long id){
+
+        if(todoRepository.existsByID(id)){
+
+                return false;
+        }
+
+        todoRepository.deleteById(id);
+
+        return true;
+
+    }
 }
